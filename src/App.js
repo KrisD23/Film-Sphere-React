b4b017ba777ae12e60908b026f06c0c882431044
Krishan Dwivedi <krishandwive9@gmail.com>
2023-11-13 18:56:57 +0530

    api key working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,19 @@
+import { useEffect } from "react";
+
+// ba5fb318
+
+const API_URL = "http://www.omdbapi.com?apikey=ba5fb318";
+
 function App() {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data);
+  };
+  useEffect(() => {
+    searchMovies("batman");
+  }, []);
   return (
     <div>
       <h1>APP</h1>
