4388f31102fcd5d832b92f58e4a043e351ebd480
Krishan Dwivedi <krishandwive9@gmail.com>
2023-11-13 19:42:08 +0530

    Basic placeholder check

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import SearchIcon from "./search.svg";
 // ba5fb318
 
 const API_URL = "http://www.omdbapi.com?apikey=ba5fb318";
+const movie1 = {
+  Title: "Batman Begins",
+  Year: "2005",
+  imdbID: "tt0372784",
+  Type: "movie",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BOTY4YjI2N2MtYmFlMC00ZjcyLTg3YjEtMDQyM2ZjYzQ5YWFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+};
 
+// App componet
 function App() {
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
@@ -18,10 +27,36 @@ function App() {
 
   return (
     <div className="app">
+      {/* Site name */}
       <h1>FilmSphere</h1>
 
+      {/* Search input */}
       <div className="search">
         <input placeholder="Movie titile" value="Batman" onChange={() => {}} />
+        <img src={SearchIcon} alt="Search" onClick={() => {}} />
+      </div>
+
+      <div className="container">
+        <div className="movie">
+          <div>
+            <p>{movie1.Year}</p>
+          </div>
+          <div>
+            <img
+              src={
+                movie1.Poster !== "N/A"
+                  ? movie1.Poster
+                  : "http://via.placeholder.com/400"
+              }
+              alt={movie1.Title}
+            />
+          </div>
+
+          <div>
+            <span>{movie1.Type}</span>
+            <h3>{movie1.Title}</h3>
+          </div>
+        </div>
       </div>
     </div>
   );
